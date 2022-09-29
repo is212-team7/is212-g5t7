60ec4ce18d6bd4a55622e9be350adeaf73067c75
h-ejia <92679281+h-ejia@users.noreply.github.com>
2022-09-29 12:55:39 +0800

    Create add-course.tsx
    Imported: Description, Code, Text, Image, Grid, Card, Button, Input, Link, Page, Spacer
    
    Added placeholder Course Image, and placeholder Course Description
    
    Added "Add Course" button

---
+++ client/pages/add-course.tsx
@@ -0,0 +1,40 @@
+import { Description, Code, Text, Image, Grid, Card, Button, Input, Link, Page, Spacer } from '@geist-ui/core';
+import type { NextPage } from 'next';
+import Navbar from './components/Navbar';
+
+const Home: NextPage = () => {
+  return (
+    <Page>
+      <Page.Content>
+        <h2>Add Course</h2>
+        <Spacer h={2} />
+        <Text b font="30px">Course Title: Service Innovation</Text> 
+        <Grid.Container  justify="center" gap={3} height="100%">
+          <Grid flex-direction="column" direction="column" xs={24} md={12}>
+                <Image height="fluid" src="https://www.bain.com/contentassets/100cd05b7a634f839e25e66cde37850e/gettyimages-1178687233-16_9.jpg" />
+          </Grid>
+
+          <Grid  direction="column" xs={24} md={12}>
+                <Text b font="20px">
+                Description:
+                </Text>
+                <Spacer/>
+                <Text>
+                This module covers knowledge and application skills in understanding the importance of service innovation and the methods and opportunities that are available for a service staff to generate ideas that contribute to service innovation.
+                </Text>
+                <Spacer/>               
+                <Spacer/>
+                <Link href="/addCourse">
+                <Button type="secondary">Add Course</Button>
+                </Link>
+          </Grid>
+        </Grid.Container>
+     
+        <Spacer />
+
+      </Page.Content>
+    </Page>
+  );
+};
+
+export default Home;
