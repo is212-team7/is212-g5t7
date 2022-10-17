0ff6ee4140a24ac98e5485fa9cbf552385c6f44f
Regina Liu <regina.liuyixuan@gmail.com>
2022-10-18 07:07:49 +0800

    refactor: dynamically render course cards

---
+++ client/database/courses.ts
@@ -0,0 +1,37 @@
+export interface Course {
+  label: string;
+  description: string;
+}
+
+export const courses = [
+  {
+    label: 'Service Innovation',
+    description:
+      'This module covers knowledge and application skills in understanding the importance of service innovation and the methods and opportunities that are available for a service staff to generate ideas that contribute to service innovation.',
+  },
+  {
+    label: 'Website Development',
+    description:
+      'This module covers knowledge and application skills in understanding the importance of service innovation and the methods and opportunities that are available for a service staff to generate ideas that contribute to service innovation.',
+  },
+  {
+    label: 'Accounting Basics',
+    description:
+      'This module covers knowledge and application skills in understanding the importance of service innovation and the methods and opportunities that are available for a service staff to generate ideas that contribute to service innovation.',
+  },
+  {
+    label: 'Marketing Tactics',
+    description:
+      'This module covers knowledge and application skills in understanding the importance of service innovation and the methods and opportunities that are available for a service staff to generate ideas that contribute to service innovation.',
+  },
+  {
+    label: 'Customer Communication',
+    description:
+      'This module covers knowledge and application skills in understanding the importance of service innovation and the methods and opportunities that are available for a service staff to generate ideas that contribute to service innovation.',
+  },
+  {
+    label: 'Stakeholder Management',
+    description:
+      'This module covers knowledge and application skills in understanding the importance of service innovation and the methods and opportunities that are available for a service staff to generate ideas that contribute to service innovation.',
+  },
+];
--- client/pages/view-courses.tsx
+++ client/pages/view-courses.tsx
@@ -1,9 +1,9 @@
-import { Grid, Card, Image, Text, Button, Input, Link, Page, Select, Spacer } from '@geist-ui/core';
+import { Card, Grid, Image, Page, Spacer, Text } from '@geist-ui/core';
 import type { NextPage } from 'next';
-
+import Link from 'next/link';
+import { Course, courses } from '../database/courses';
 
 const ViewCourses: NextPage = () => {
-
   return (
     <Page>
       <Page.Content>
@@ -12,51 +12,40 @@ const ViewCourses: NextPage = () => {
         <Spacer h={2} />
 
         <Grid.Container gap={2} height="100px">
-        <Grid xs={24} md={6}>
-          <Card onClick={onClick} style={{ cursor: "pointer" }} shadow width="100%" href="./add-course">
-          <Image src="https://user-images.githubusercontent.com/11304944/76085431-fd036480-5fec-11ea-8412-9e581425344a.png"
-          height="200px" width="400px" draggable={false} />
-          <Text h4 mb={0}>Service Innovation</Text>
-          <Text type="secondary" small> This module covers knowledge and application skills in understanding the importance of service innovation and the methods and opportunities that are available for a service staff to generate ideas that contribute to service innovation.</Text>
-        </Card>
-
-        </Grid>
-        <Grid xs={24} md={6}>
-          <Card shadow width="100%">
-          <Image src="https://user-images.githubusercontent.com/11304944/76085431-fd036480-5fec-11ea-8412-9e581425344a.png"
-          height="200px" width="400px" draggable={false} />
-          <Text h4 mb={0}>Service Innovation</Text>
-          <Text type="secondary" small> This module covers knowledge and application skills in understanding the importance of service innovation and the methods and opportunities that are available for a service staff to generate ideas that contribute to service innovation.</Text>
-        </Card>
-        </Grid>
-        <Grid xs={24} md={6}>
-          <Card shadow width="100%">
-          <Image src="https://user-images.githubusercontent.com/11304944/76085431-fd036480-5fec-11ea-8412-9e581425344a.png"
-          height="200px" width="400px" draggable={false} />
-          <Text h4 mb={0}>Service Innovation</Text>
-          <Text type="secondary" small> This module covers knowledge and application skills in understanding the importance of service innovation and the methods and opportunities that are available for a service staff to generate ideas that contribute to service innovation.</Text>
-        </Card>
-        </Grid>
-        <Grid xs={24} md={6}>
-          <Card shadow width="100%">
-          <Image src="https://user-images.githubusercontent.com/11304944/76085431-fd036480-5fec-11ea-8412-9e581425344a.png"
-          height="200px" width="400px" draggable={false} />
-          <Text h4 mb={0}>Service Innovation</Text>
-          <Text type="secondary" small> This module covers knowledge and application skills in understanding the importance of service innovation and the methods and opportunities that are available for a service staff to generate ideas that contribute to service innovation.</Text>
-        </Card>
-        </Grid>
+          {courses.map(({ label, description }) => (
+            <CourseCard key={label} label={label} description={description} />
+          ))}
         </Grid.Container>
-
-   
-
-      
       </Page.Content>
     </Page>
   );
 };
 
 // SUB-COMPONENTS
-const onClick = () => {
-  location.href = "./add-course";
-}
+
+const CourseCard = ({ label, description }: Course) => {
+  return (
+    <Link href="./add-course">
+      <Grid xs={6}>
+        <Card style={{ cursor: 'pointer' }} shadow width="100%">
+          <Image
+            src="https://user-images.githubusercontent.com/11304944/76085431-fd036480-5fec-11ea-8412-9e581425344a.png"
+            height="200px"
+            width="400px"
+            draggable={false}
+            alt={label}
+          />
+          <Text h4 mb={0}>
+            {label}
+          </Text>
+          <Text type="secondary" small>
+            {' '}
+            {description}
+          </Text>
+        </Card>
+      </Grid>
+    </Link>
+  );
+};
+
 export default ViewCourses;
