f67790a599d526ffd25bc681486bc8ba0ea888f4
tj <40231283+chennnny@users.noreply.github.com>
2022-10-17 18:32:59 +0800

    feat: add view skill page (#7)
    * feat: add view skill page
    
    * refactor: dynamically render skills
    
    * chore: rebase
    
    Co-authored-by: Tianjing, Regina Liu <regina.liuyixuan@gmail.com>

--- client/database/skills.ts
+++ client/database/skills.ts
@@ -1,44 +1,62 @@
-export const skills = {
+export interface SkillCategory {
+  [skillCategory: string]: Skill[];
+}
+
+export interface Skill {
+  label: string;
+  description: string;
+}
+
+export const skillCategories: SkillCategory = {
   sales: [
-    'Product Knowledge',
-    'Lead Qualification',
-    'Lead Prospecting ',
-    'Customer Needs Analysis',
-    'Referral Marketing',
-    'Contract Negotiation',
-    'CRM Software (Salesforce, Hubspot, Zoho, Freshsales)',
-    'POS Skills',
-    'Cashier Skills',
+    { label: 'Product Knowledge', description: 'Description is empty.' },
+    { label: 'Lead Qualification', description: 'Description is empty.' },
+    { label: 'Lead Prospecting ', description: 'Description is empty.' },
+    { label: 'Customer Needs Analysis', description: 'Description is empty.' },
+    { label: 'Referral Marketing', description: 'Description is empty.' },
+    {
+      label: 'CRM Software (Salesforce, Hubspot, Zoho, Freshsales)',
+      description: 'Description is empty.',
+    },
+    { label: 'POS Skills', description: 'Description is empty.' },
+    { label: 'Cashier Skills', description: 'Description is empty.' },
+    {
+      label: 'Contract Negotiation',
+      description: 'Description is empty.',
+    },
   ],
   technology: [
-    'Web Development',
-    'Data Structures',
-    'Security',
-    'Machine Learning',
-    'UX/UI',
-    'Cloud Management',
-    'Agile Development',
+    { label: 'Web Development', description: 'Description is empty.' },
+    { label: 'Data Structures', description: 'Description is empty.' },
+    { label: 'Security', description: 'Description is empty.' },
+    { label: 'Machine Learning', description: 'Description is empty.' },
+    { label: 'UX/UI', description: 'Description is empty.' },
+    { label: 'Cloud Management', description: 'Description is empty.' },
+    { label: 'Agile Development', description: 'Description is empty.' },
   ],
   marketing: [
-    'SEO/SEM',
-    'A/B Testing',
-    'Social Media Marketing and Paid Social Media Advertising',
-    'Sales Funnel Management',
-    'CMS Tools',
-    'Graphic Design Skills',
-    'Email Marketing Skills',
-    'Email Automation',
-    'Data Visualization',
-    'Typography',
-    'Print Design',
-    'Photography and Branding',
+    { label: 'SEO/SEM', description: 'Description is empty.' },
+    { label: 'A/B Testing', description: 'Description is empty.' },
+    {
+      label: 'Social Media Marketing and Paid Social Media Advertising',
+      description: 'Description is empty.',
+    },
+    { label: 'Sales Funnel Management', description: 'Description is empty.' },
+    { label: 'CMS Tools', description: 'Description is empty.' },
+    { label: 'Graphic Design Skills', description: 'Description is empty.' },
+    { label: 'Email Marketing Skills', description: 'Description is empty.' },
+    { label: 'Email Automation', description: 'Description is empty.' },
+    { label: 'Data Visualization', description: 'Description is empty.' },
+    { label: 'Typography', description: 'Description is empty.' },
+    { label: 'Print Design', description: 'Description is empty.' },
+    { label: 'Photography and Branding', description: 'Description is empty.' },
   ],
   admin: [
-    'Data Entry',
-    'Billing',
-    'Scheduling',
-    'Microsoft Office',
-    'Shipping',
-    'Salesforce',
+    { label: 'Data Entry', description: 'Description is empty.' },
+    { label: 'Billing', description: 'Description is empty.' },
+    { label: 'Scheduling', description: 'Description is empty.' },
+    { label: 'Microsoft Office', description: 'Description is empty.' },
+    { label: 'Shipping', description: 'Description is empty.' },
+    { label: 'Salesforce', description: 'Description is empty.' },
   ],
 };
