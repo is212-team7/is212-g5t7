2e48d91d87a53b5a9d85aaf05c440ef6458ee33b
bayberryy <yvonne881881@yahoo.com.sg>
2022-10-18 08:08:09 +0800

    Update view-role.tsx

--- client/pages/view-role.tsx
+++ client/pages/view-role.tsx
@@ -1,81 +1,62 @@
-import { Button, Input, Link, Page, Spacer, Card, Text, Divider, Grid } from '@geist-ui/core';
+import { Card, Divider, Grid, Page, Spacer, Text } from '@geist-ui/core';
 import type { NextPage } from 'next';
-import { roles } from './database/roles';
+import { Role, roleCategories } from '../database/roles';
 
-const Home: NextPage = () => {
+const ViewRolePage: NextPage = () => {
+  const RoleCategory = ({
+    key,
+    roleCategory,
+    roleDetails,
+  }: {
+    key: string;
+    roleCategory: string;
+    roleDetails: Role[];
+  }) => {
     return (
-        <Page>
-            <Page.Content>
-                <h2>Roles</h2>
-                <Spacer h={2} />
-                <Grid.Container gap={1.5}>
-                    {/* having some issue writing a loop here */}
-                    <Spacer h={2}/>
-                    <Grid justify="center">
-                    <h4> {Object.keys(roles)[0]}</h4>
-                        {Object.values(roles)[0].map(role => (
-                            <Card width="600px">
-                                <Card.Content>
-                                    <Text b my={0}>{role}</Text>
-                                </Card.Content>
-                                <Divider h="1px" my={0} />
-                                <Card.Content>
-                                    <Text>some role description here.</Text>
-                                </Card.Content>
-                            </Card>
-                        ))}
-                    </Grid>
-                    <Spacer h={2} />
-                    <Grid  justify="center">
-                    <h4>Technology</h4>
-                        {skills['technology'].map(skill => (
-                            <Card width="600px">
-                                <Card.Content>
-                                    <Text b my={0}>{skill}</Text>
-                                </Card.Content>
-                                <Divider h="1px" my={0} />
-                                <Card.Content>
-                                    <Text>some skill description here.</Text>
-                                </Card.Content>
-                            </Card>
-                        ))}
-                    </Grid>
-                    <Spacer h={2} />
-                    
-                    <Grid justify="center">
-                    <h4>Marketing</h4>
-                        {skills['marketing'].map(skill => (
-                            <Card width="600px">
-                                <Card.Content>
-                                    <Text b my={0}>{skill}</Text>
-                                </Card.Content>
-                                <Divider h="1px" my={0} />
-                                <Card.Content>
-                                    <Text>some skill description here.</Text>
-                                </Card.Content>
-                            </Card>
-                        ))}
-                    </Grid>
-                    <Grid justify="center">
-                    <h4>Admin</h4>
-                        {skills['admin'].map(skill => (
-                            <Card width="600px">
-                                <Card.Content>
-                                    <Text b my={0}>{skill}</Text>
-                                </Card.Content>
-                                <Divider h="1px" my={0} />
-                                <Card.Content>
-                                    <Text>some skill description here.</Text>
-                                </Card.Content>
-                            </Card>
-                        ))}
-                    </Grid>
-                </Grid.Container>
+      <Grid justify="center" key={key}>
+        <h4>
+          {roleCategory.slice(0, 1).toUpperCase() + roleCategory.slice(1)}
+        </h4>
 
-
-            </Page.Content>
-        </Page>
+        {roleDetails.map(({ label, description }) => (
+          <>
+            <Card width="600px" key={label}>
+              <Card.Content>
+                <Text b my={0}>
+                  {label}
+                </Text>
+              </Card.Content>
+              <Divider h="1px" my={0} />
+              <Card.Content>
+                <Text>{description}</Text>
+              </Card.Content>
+            </Card>
+            <Spacer h={2} />
+          </>
+        ))}
+      </Grid>
     );
+  };
+
+  return (
+    <Page>
+      <Page.Content>
+        <h2>Roles</h2>
+        <Spacer h={2} />
+        <Grid.Container gap={1.5}>
+          {Object.entries(roleCategories).map(
+            ([roleCategory, roleDetails]) => (
+              <RoleCategory
+                key={roleCategory}
+                roleCategory={roleCategory}
+                roleDetails={roleDetails}
+              />
+            )
+          )}
+        </Grid.Container>
+      </Page.Content>
+    </Page>
+  );
 };
 
-export default Home;
+export default ViewRolePage;
