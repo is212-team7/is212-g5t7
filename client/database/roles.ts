66ce3e9aed921b52f7a551431a5f68fc997b7624
bayberryy <89002933+bayberryy@users.noreply.github.com>
2022-10-18 08:30:30 +0800

    feat: add view-role page (#9)
    * Add view-role page
    
    * Update view-role.tsx
    
    * update: view-role frontend

--- client/database/roles.ts
+++ client/database/roles.ts
@@ -1,26 +1,25 @@
 export interface RoleCategory {
-    [roleCategory: string]: Role[];
-  }
-  
-  export interface Role {
-    label: string;
-    description: string;
-  }
-  
-  export const roleCategories: RoleCategory = {
-    sales: [
-      { label: 'Sales Associate', description: 'Description is empty.' },
-      { label: 'Sales Representative', description: 'Description is empty.' },
-      { label: 'Account Executive', description: 'Description is empty.' },
-      { label: 'Sales Manager', description: 'Description is empty.' },
-      { label: 'Salesperson', description: 'Description is empty.' },
-    ],
-    technology: [
-      { label: 'Data Scientist', description: 'Description is empty.' },
-      { label: 'Software Developer', description: 'Description is empty.' },
-      { label: 'Web Developer', description: 'Description is empty.' },
-      { label: 'Sales Engineer', description: 'Description is empty.' },
-      { label: 'Data Analyst', description: 'Description is empty.' },
-    ]
-  };
-  
+  [roleCategory: string]: Role[];
+}
+
+export interface Role {
+  label: string;
+  description: string;
+}
+
+export const roleCategories: RoleCategory = {
+  sales: [
+    { label: 'Sales Associate', description: 'Description is empty.' },
+    { label: 'Sales Representative', description: 'Description is empty.' },
+    { label: 'Account Executive', description: 'Description is empty.' },
+    { label: 'Sales Manager', description: 'Description is empty.' },
+    { label: 'Salesperson', description: 'Description is empty.' },
+  ],
+  technology: [
+    { label: 'Data Scientist', description: 'Description is empty.' },
+    { label: 'Software Developer', description: 'Description is empty.' },
+    { label: 'Web Developer', description: 'Description is empty.' },
+    { label: 'Sales Engineer', description: 'Description is empty.' },
+    { label: 'Data Analyst', description: 'Description is empty.' },
+  ]
+};
