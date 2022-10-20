43eff3ebd3dbb6338a72e92224d0f9c76c72979c
szekee <tewszekee@gmail.com>
2022-10-20 09:28:08 +0800

    refactor: shift getskillsbyroleid endpoint under skills routes

--- server/src/routes/skills.ts
+++ server/src/routes/skills.ts
@@ -1,4 +1,4 @@
-import { Skill } from '@lib/models';
+import { RoleSkill, Skill } from '@lib/models';
 import { celebrate, Joi } from 'celebrate';
 import { Router } from 'express';
 
@@ -138,6 +138,36 @@ skills.delete(
             } else {
                 await skill.destroy();
                 res.json({ message: 'Skill deleted' });
+            }
+        } catch (error) {
+            res.status(400).json(error.message);
+        }
+    }
+);
+
+
+
+
+// Get skills by role
+skills.get(
+    '/role/:Role_ID',
+    celebrate({ params: { Role_ID: Joi.number().required() } }),
+    async (req, res) => {
+        try {
+            const skill_ids = await RoleSkill.findAll({
+                where: { Role_ID: req.params.Role_ID },
+                attributes: ['Skill_ID'],
+            });
+            if (!skill_ids) {
+                return res.status(404).json({ error: 'Role not found' });
+            } else {
+                let result: any[] = [];
+                for (let i = 0; i < skill_ids.length; i++) {
+                    let skill_id = skill_ids[i]['Skill_ID'];
+                    let skill = await Skill.findByPk(skill_id);
+                    result.push(skill);
+                }
+                res.json(result);
             }
         } catch (error) {
             res.status(400).json(error.message);
