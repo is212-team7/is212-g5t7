6e628a4353fbc58ac743733c8daf83cb75c2b7f7
tj <40231283+chennnny@users.noreply.github.com>
2022-10-24 21:00:04 +0800

    view skill with backend (#40)
    * view skill with backend
    
    * fix: make description property type optional

--- client/pages/admin/skills.tsx
+++ client/pages/admin/skills.tsx
@@ -1,43 +1,55 @@
-import { Card, Divider, Grid, Page, Spacer, Text } from '@geist-ui/core';
+import { Card, Divider, Grid, Link, Page, Spacer, Text } from '@geist-ui/core';
 import type { NextPage } from 'next';
-import { Skill, skillCategories } from '../../database/skills';
+import { useEffect, useState } from 'react';
+import { Skill } from '../api/skills';
 
-const ViewSkillPage: NextPage = () => {
-    const SkillCategory = ({
-        key,
-        skillCategory,
-        skillDetails,
+const AdminSkillsPage: NextPage = () => {
+    const [skills, setSkills] = useState<Skill[]>();
+
+    const Skill = ({
+        id,
+        name,
+        description,
     }: {
-        key: string;
-        skillCategory: string;
-        skillDetails: Skill[];
+        id: number;
+        name: string;
+        description?: string;
     }) => {
         return (
             <Grid justify="center">
-                <h4>
-                    {skillCategory.slice(0, 1).toUpperCase() +
-                        skillCategory.slice(1)}
-                </h4>
-
-                {skillDetails.map(({ label, description }) => (
-                    <>
-                        <Card width="600px" key={label}>
-                            <Card.Content>
-                                <Text b my={0}>
-                                    {label}
-                                </Text>
-                            </Card.Content>
-                            <Divider h="1px" my={0} />
-                            <Card.Content>
-                                <Text>{description}</Text>
-                            </Card.Content>
-                        </Card>
-                        <Spacer h={2} />
-                    </>
-                ))}
+                <Card width="600px" key={name}>
+                    <Card.Content>
+                        <Text b my={0}>
+                            {name}
+                        </Text>
+                    </Card.Content>
+                    <Divider h="1px" my={0} />
+                    <Card.Content>
+                        <Text font="12px" mt={0} style={{ color: '#ccc' }}>
+                            DESCRIPTION
+                        </Text>
+                        <Text>{description}</Text>
+                    </Card.Content>
+                    <Card.Footer>
+                        <Link color href={'enroll/' + id}>
+                            View skills for {name} &#8594;
+                        </Link>
+                    </Card.Footer>
+                </Card>
+                <Spacer h={2} />
             </Grid>
         );
     };
+
+    useEffect(() => {
+        fetch('/api/skills', { method: 'GET' })
+            .then((response) => response.json())
+            .then((result) => {
+                console.log({ result });
+                setSkills(result);
+            })
+            .catch((error) => console.log('error', error));
+    }, []);
 
     return (
         <Page>
@@ -45,19 +57,19 @@ const ViewSkillPage: NextPage = () => {
                 <h2>Skills</h2>
                 <Spacer h={2} />
                 <Grid.Container gap={1.5}>
-                    {Object.entries(skillCategories).map(
-                        ([skillCategory, skillDetails]) => (
-                            <SkillCategory
-                                key={skillCategory}
-                                skillCategory={skillCategory}
-                                skillDetails={skillDetails}
+                    {skills &&
+                        skills.map(({ id, name, description }) => (
+                            <Skill
+                                key={name}
+                                id={id}
+                                name={name}
+                                description={description}
                             />
-                        )
-                    )}
+                        ))}
                 </Grid.Container>
             </Page.Content>
         </Page>
     );
 };
 
-export default ViewSkillPage;
+export default AdminSkillsPage;
