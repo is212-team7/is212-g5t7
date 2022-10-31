fb61c946a599d1d08218d349ecdef582de185ac6
Regina Liu <regina.liuyixuan@gmail.com>
2022-10-31 22:48:22 +0800

    fix: re-added deleted part of GET request

--- server/src/routes/courses.ts
+++ server/src/routes/courses.ts
@@ -44,15 +44,27 @@ courses.get(
 
 // view all courses that can be fulfilled by a skill
 courses.get(
-    '/Skill/:Skill_ID',
-    celebrate({ params: { Skill_ID: Joi.number().required() } }),
+    '/:courseId',
+    celebrate({
+        params: {
+            courseId: Joi.string().required(),
+        },
+    }),
     async (req, res) => {
         try {
-            const skill = await Skill.findByPk(req.params.Skill_ID);
+            const course = await Course.findOne({
+                where: { Course_ID: req.params.courseId },
+            });
 
-            if (skill == null) {
-                return res.status(404).json({ error: 'Skill not found' });
+            if (!course) {
+                throw new Error(`Course does not exist`);
             }
+            res.json(course);
+        } catch (error) {
+            res.status(400).json(error.message);
+        }
+    }
+);
 
 // assign course to skill
 courses.post(
@@ -70,7 +82,7 @@ courses.post(
             if (skill == null) {
                 return res.status(404).json({ error: 'Skill not found' });
             }
-            
+
             // check if course exists
             const course: any = await Course.findByPk(req.params.courseId);
             if (course == null) {
@@ -81,13 +93,11 @@ courses.post(
             await skill.addCourse(course);
 
             return res.json('Sucessfully added course to skill');
-
         } catch (error) {
             res.status(400).json(error.message);
         }
     }
 );
-
 
 // delete course-skill assignment
 courses.delete(
